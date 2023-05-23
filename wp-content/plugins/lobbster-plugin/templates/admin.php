5372a07ae763712c7d7b94f78c0260f740d0fb3d
Saša Kojadinović <sasa.kojadinovic@lobbster.co>
2023-05-23 20:16:38 +0200

    Update 6.22

---
+++ wp-content/plugins/lobbster-plugin/templates/admin.php
@@ -0,0 +1,1 @@
+<h1>Lobbster Plugin Admin Page</h1>
---
+++ wp-content/plugins/lobbster-plugin/uninstall.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package LobbsterPlugin
+ */
+
+
+if( !defined('WP_UNINSTALL_PLUGIN')){
+    die;
+}
+
+$books = get_posts(array('post_type' =>'book', 'numberposts' => -1));
+
+foreach($books as $book) {
+    wp_delete_post($book->ID, true);
+}
